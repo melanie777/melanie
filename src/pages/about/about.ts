337fd88c534b806c7570fa65efef6c3d05ef3df3
melanie <meli_sadey@hotmail.com>
2017-11-18 12:06:07 -0500

    agregue variables

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,7 +6,25 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  
+
+   nombre: string = "melanie Cruz";
+   ocupacion: string ="estudiante";
+  edad: number = 16;
+  datos: any = {
+   nombre: "sadey cruz",
+    ocupacion: "estudiante",
+    edad: 16
+  };
+    dato2: any = {
+    origen: "ayacucho peru",
+    numero: "968314292",
+    email: "meli_sadey@hotail.com"
+
+
+
+    
+
+  };
   constructor(public navCtrl: NavController) {
 
   }
