09ab270b86a00c46417eb02b98eab3f372199051
melanie <meli_sadey@hotmail.com>
2017-12-16 11:00:30 -0500

    filtrado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -68,6 +68,12 @@ export class HomePage implements OnInit {
          texto:"todos"
        }
    ];
+
+   filtro:string="todos";
+   actualizarQuehacer(checked:boolean,componente){
+     componente.done=checked;
+   }
+
   constructor(
     public navCtrl: NavController,
     private todoService: TodoService) {}
@@ -78,8 +84,18 @@ export class HomePage implements OnInit {
     this.agregarQuehacer("armar el cubo de rubik",true);
     // codigo al iniciar el componente
   }
-  agregarQuehacer(nuevoQuehacer: string, terminado: boolean){
-    if (nuevoQuehacer !== ""){
+  listarQuehaceres (){
+    if(this.filtro=="todos"){
+      return this.quehaceres;
+
+    }else if(this.filtro ==="pendientes"){
+      return this.quehaceres.filter(quehacer=>!quehacer.done);
+    }else if(this.filtro ==="terminados"){
+        return this.quehaceres.filter(quehacer=>quehacer.done);
+  }
+}
+  agregarQuehacer(nuevoQuehacer: String, terminado: boolean){
+    if (nuevoQuehacer !==""){
 
 
     this.quehaceres.push({
@@ -91,14 +107,17 @@ export class HomePage implements OnInit {
 }
   filtrarQuehaceres(texto: string) {
    if(texto==="terminados"){
+     this.filtro="terminados";
     this.botones[0].grande = true;
     this.botones[1].grande =false;
     this.botones[2].grande =false;
  }else if (texto==="pendientes") {
+    this.filtro="pendientes";
    this.botones[0].grande =false;
    this.botones[1].grande =true;
    this.botones[2].grande =false;
  }else if(texto==="todos") {
+   this.filtro="todos";
  this.botones[0].grande =false;
  this.botones[1].grande =false;
  this.botones[2].grande =true;
